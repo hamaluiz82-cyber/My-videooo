8c569ac1fd66a1de990753cbd6999c86efb9d428
hamaluiz82-cyber <hamaluiz82@gmail.com>
2026-05-08 14:37:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,32 @@ import os
 import requests
 import time
 
+def load_users():
+    try:
+        with open("users.txt", "r") as f:
+            return set(f.read().splitlines())
+    except:
+        return set()
+
+users = load_users()
+
 TOKEN = os.getenv("TOKEN")
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
+user_id = str(message.from_user.id)
 
+    if user_id not in users:
+        users.add(user_id)
+
+        with open("users.txt", "a") as f:
+            f.write(user_id + "\n")
+
+    bot.reply_to(
+        message,
+        f"👋 Welcome\n👥 Users: {len(users)}"
     text = """
 👋 بەخێربێیت بۆ بۆتی داونلۆدی ڤیدیۆ
 
@@ -60,9 +79,9 @@ def download_video(message):
 
         with open(filename, 'rb') as video:
 
-            bot.send_video(message.chat.id, video)
+            bot.send_video(message.chat.id, open(filename, 'rb'))
 
-        os.remove(filename)
+os.remove(filename)
 
     except Exception as e:
 
